b59d781101dc3d3b8a86daffed06bb33ad0f4a35
honghyeong <71376151+honghyeong@users.noreply.github.com>
2021-01-12 12:30:29 +0900

    Update SelectionSort.py

--- Base/Sort/SelectionSort.py
+++ Base/Sort/SelectionSort.py
@@ -9,7 +9,7 @@ for _ in range(N):
 
 for i in range(len(numbers)-1): # n-1개 만 정렬하면, 나머지 1개는 자동 정렬
     maxIdx=i
-    for j in range(i,len(numbers)):
+    for j in range(i+1,len(numbers)):
         if numbers[j] < numbers[maxIdx]:    # 가장 작은 값 탐색
             maxIdx=j
     numbers[i],numbers[maxIdx]=numbers[maxIdx],numbers[i]

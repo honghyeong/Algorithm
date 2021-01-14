3423e609a750cf5e92984a3582dd0879ad83ba7b
honghyeong <71376151+honghyeong@users.noreply.github.com>
2021-01-14 14:11:01 +0900

    Create 2108_Counter.py

---
+++ PYTHON/SILVER4/2108_Counter.py
@@ -0,0 +1,24 @@
+import sys
+from collections import Counter
+
+input=sys.stdin.readline
+N=int(input())
+nums=[]
+for _ in range(N):
+    nums.append(int(input()))
+
+nums.sort()
+nums_mode=Counter(nums).most_common()
+
+print(round(sum(nums)//N))
+print(nums[N//2])
+
+if len(nums)!=1:
+    if nums_mode[0][1]==nums_mode[1][1]:
+        print(nums_mode[1][0])
+    else:
+        print(nums_mode[0][0])
+else:
+    print(nums_mode[0][0])
+
+print(nums[-1]-nums[0])

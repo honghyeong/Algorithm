2324938711c3a218a65cf395320157c6bc4d6555
honghyeong <71376151+honghyeong@users.noreply.github.com>
2021-01-19 21:28:57 +0900

    Update most_common_word.py

--- LeetCode/practice/string_manipulation/most_common_word.py
+++ LeetCode/practice/string_manipulation/most_common_word.py
@@ -1,5 +1,25 @@
 # 819
 
+# Using Default dict
+
+import collections
+import re
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        # extraction lower words from paragraph
+        words=[word for word in re.sub('[^\w]',' ',paragraph).lower().split() if word not in banned]
+        
+        
+        # using default dictionary
+        counts=collections.defaultdict(int)
+        for word in words:
+            counts[word]+=1
+            
+        return max(counts,key=counts.get)
+
+
+
+
 # Using Counter, regular expression, list comprehension
 
 class Solution:

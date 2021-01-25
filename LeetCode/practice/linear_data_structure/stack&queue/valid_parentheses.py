8885b80c036e25d534135bf995e385c366fb16b6
honghyeong <71376151+honghyeong@users.noreply.github.com>
2021-01-26 00:06:30 +0900

    Update valid_parentheses.py

--- LeetCode/practice/linear_data_structure/stack&queue/valid_parentheses.py
+++ LeetCode/practice/linear_data_structure/stack&queue/valid_parentheses.py
@@ -7,8 +7,8 @@ class Solution:
                "]":'['}
 
         for char in s:
-            if char not in stack:
-                stack.append(stack)
+            if char not in table:
+                stack.append(char)
             elif not stack or table[char]!=stack.pop():
                 return False
         return len(stack)==0

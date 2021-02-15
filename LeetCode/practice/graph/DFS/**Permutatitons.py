c8b3cd0cf9ca8a07e3342e39d669dee8117b6312
honghyeong <71376151+honghyeong@users.noreply.github.com>
2021-02-15 09:48:04 +0900

    Update **Permutatitons.py

--- LeetCode/practice/graph/DFS/**Permutatitons.py
+++ LeetCode/practice/graph/DFS/**Permutatitons.py
@@ -1,5 +1,5 @@
 # 46
-
+# DFS, Backtracking
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         results=[]
@@ -21,3 +21,10 @@ class Solution:
                 
         dfs(nums)
         return results
+
+    
+# Itertools
+import itertools
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        return list(map(list,itertools.permutations(nums)))

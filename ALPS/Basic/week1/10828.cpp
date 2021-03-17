e9b0609983cf2d19e2af39d5d7e249fc3eadb49a
honghyeong <71376151+honghyeong@users.noreply.github.com>
2021-03-18 05:35:04 +0900

    Update 10828.cpp

--- ALPS/Basic/week1/10828.cpp
+++ ALPS/Basic/week1/10828.cpp
@@ -1,1 +1,58 @@
-d
+#include<iostream>
+#include<stack>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+
+	int N;
+	cin >> N;
+
+	string str;
+	stack<int> st;
+
+	for (int i = 0; i < N; i++)	{
+		cin >> str;
+		if (str == "push") {
+			int num;
+			cin >> num;
+			st.push(num);
+		}
+		else if (str == "pop") {
+			if (!st.empty()) {
+				cout << st.top() << endl;
+				st.pop();
+			}
+			else
+			{
+				cout << "-1" << endl;
+			}
+		}
+		else if (str == "size") {
+			cout << st.size() << endl;
+		}
+		else if (str == "empty") {
+			if (!st.empty()) {
+				cout << "0" << endl;
+			}
+			else {
+				cout << "1" << endl;
+			}
+		}
+		else if (str == "top") {
+			if (!st.empty()) {
+				cout << st.top() << endl;
+
+			}
+			else
+			{
+				cout << "-1" << endl;
+			}
+		}
+
+	}
+
+	return 0;
+}
